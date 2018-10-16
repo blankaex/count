b6a4e4bd0258f607195667a5d8dfa29066e05b75
blankaex <nightmare_blankaex@hotmail.com>
2018-10-16 18:57:24 +1100

    working bootstrap

--- js/script.js
+++ js/script.js
@@ -4,26 +4,6 @@ var app = new Vue({
         message: 'Count'
     },
 	methods: {
-		showView: function(id) {
-            var leaders = ["elf", "royal", "witch", "dragon", "necromancer", "vampire", "bishop", "nemesis"]
-
-            for(i = 0; i < leaders.length; i++)
-                if(leaders[i] != id)
-                    document.getElementById(leaders[i]).style.display = "none"
-
-            if(document.getElementById(id).style.display === "none")
-            {
-                document.getElementById(id).style.display = "flex"
-                document.getElementById("home").style.display = "none"
-                document.getElementById("reset").style.display = "block"
-            }
-            else
-            {
-                document.getElementById(id).style.display = "none"
-                document.getElementById("home").style.display = "block"
-                document.getElementById("reset").style.display = "none"
-            }
-		},
 		inc: function(id) {
             document.getElementById(id).innerText++
 		},
@@ -32,12 +12,9 @@ var app = new Vue({
                 document.getElementById(id).innerText--
 		},
 		reset: function() {
-            console.log("hi")
             var counters = document.getElementsByClassName("counter")
             for(i = 0; i < counters.length; i++)
-            {
                 counters[i].innerText = 0
-            }
 		}
   	}
 })
