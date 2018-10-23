14a6f7e6812af629a6598e7a63a3b1494638aba3
blankaex <nightmare_blankaex@hotmail.com>
2018-10-23 17:39:03 +1100

    mvp

--- js/script.js
+++ js/script.js
@@ -18,6 +18,26 @@ var app = new Vue({
                     counters[i].innerText = 10
                 else
                     counters[i].innerText = 0
-		}
+		},
+        view: function(id) {
+            var e = document.getElementById(id)
+            if(e.classList.contains("active")) {
+                document.getElementById("home").style.display = ""
+                document.getElementById("reset").style.display = "none"
+                e.classList.remove("active")
+                e.classList.remove("show")
+            }
+            else {
+                document.getElementById("home").style.display = "none"
+                document.getElementById("reset").style.display = "block"
+                var counters = document.getElementsByClassName("tab-pane")
+                for(i = 0; i < counters.length; i++) {
+                    counters[i].classList.remove("active")
+                    counters[i].classList.remove("show")
+                }
+                e.classList.add("active")
+                e.classList.add("show")
+            }
+        }
   	}
 })
